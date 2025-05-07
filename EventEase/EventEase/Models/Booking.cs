0303f47758c1791a271c2d91cb09196aa5086828
ST10061509-Mohammed-Moosa <127108934+ST10061509-Mohammed-Moosa@users.noreply.github.com>
2025-05-07 12:36:24 +0200

    Update Booking.cs

--- EventEase/EventEase/Models/Booking.cs
+++ EventEase/EventEase/Models/Booking.cs
@@ -1,8 +1,3 @@
-// Code Attribution:
-// 1. ASP.NET Core MVC Foreign Key Relationships with Navigation Properties — rynop — https://stackoverflow.com/questions/43803833/asp-net-core-mvc-foreign-key-relationships-with-navigation-properties
-// 2. Using Data Annotations for Email and Date Validation in ASP.NET Core — Fei Han — https://stackoverflow.com/questions/42083954/how-to-use-data-annotations-for-model-validation-in-asp-net-core
-
-
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;  // ✅ Needed for [ValidateNever]
